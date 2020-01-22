3b660527f095903c28700dfef52d830d5e7452fe
yarallira <yarallira555@gmail.com>
2020-01-22 18:05:34 -0300

    Crude em node

---
+++ src/controllers/cliente.js
@@ -0,0 +1,103 @@
+const Express = require('express');
+const Cliente = require('../models/clientesModels');
+
+const Router = Express.Router();
+
+// Pesquisar todos
+Router.get('/', async (req, res) => {
+ const id = req.params.id;
+ try{
+
+    const cliente = await Cliente.find();
+
+    if (!cliente)
+    return res.status(400).send({erro: "Cliente não encontrado"})
+
+    console.log(cliente);
+        return res.send(cliente);
+
+ }catch (erro){
+     return res.status(400).send({erro : "Erro ao encontrar cliente "})
+ }
+});
+// Cadastrar
+Router.post('/', async (req, res) => {
+    try{
+    const nome = await Cliente.create(req.body);
+
+    console.log(nome);
+    return res.send(nome);
+
+
+    }catch (erro) {
+        console.log(erro);
+        return res.status(400).send({erro : "Não foi possível cadastrar novo cliente" })
+    }
+});
+// Consultar pelo ID
+Router.get('/:id', async (req, res) => {
+    try{
+        const id = req.params.id;
+
+        const cliente = await Cliente.findById(id);
+
+        if (!cliente)
+        return res.status(400).send({erro : 'Cliente não encontrado'});
+
+        return res.send(cliente);
+
+    }catch (erro){
+        return res.status(400).send({erro: "Mão foi possivel consultar o cliente"});
+    }
+})
+// Consultar pelo nome
+Router.get('/:nome', async (req, res) => {
+    try{
+        const nome = req.params.nome;
+        console.log(nome);
+        const cliente = await Cliente.findOne(nome);
+        console.log(nome);
+        if (!cliente)
+        return res.status(400).send({erro : "Cliente não encontrado"})
+        console.log(nome);
+        return res.send(cliente);
+
+    }catch (erro){
+        return res.status(400).send({erro : "Não foi possível consultar o cliente pelo nome"})
+    }
+})
+// Atualizar
+Router.put('/:id' , async (req, res) => {
+ try{
+     const id = req.params.id;
+     const {nome, rg, cpf, idade, telefone , email} = req.body;
+
+     const cliente = await Cliente.findByIdAndUpdate(id , {$set: {nome, rg, cpf, idade, telefone , email}} , {new : true})
+     console.log(cliente);
+     return res.send(cliente);
+
+ } catch (erro){
+    return res.status(400).send({erro: "Mão foi possivel atualizar o cliente"});
+ }
+})
+
+// deletar
+
+Router.delete('/:id' , async (req, res) => {
+    try{
+        const id = req.params.id
+
+        const cliente = await Cliente.findByIdAndRemove(id);
+
+        if (!id)
+        return res.status(400).send({erro : "Id não foi encontrado"})
+        console.log(cliente);
+        
+        return res.send("Cliente removido com sucesso!")
+
+    }catch (erro){
+        return res.status(400).send({ erro : 'Não foi possível deletear o cliente'});
+    }
+})
+
+module.exports = app => app.use('/Cliente', Router);
---
+++ src/models/clientesModels.js
@@ -0,0 +1,33 @@
+const mongoose = require('../database');
+
+const ClienteSchema = new mongoose.Schema({
+    nome: {
+        type: String,
+        required: false,
+    },
+    rg: {
+        type: String,
+        required: true,
+    },
+    cpf: {
+        type: String,
+        required: true,
+    },
+    idade: {
+        type: Number,
+        required: true,
+    },
+    telefone: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+
+})
+
+const Cliente = mongoose.model('Cliente', ClienteSchema);
+
+module.exports = Cliente;
